92f07c3a959c54351212a163d6c69bb5c109b9b8
Sabarinathan Mani <mani.sabarinathan@gmail.com>
2022-07-29 15:43:36 -0700

    OAUTH [Sabarinathan] oauth with git authorization

---
+++ src/test/java/com/oauth/demo/OAuthGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
